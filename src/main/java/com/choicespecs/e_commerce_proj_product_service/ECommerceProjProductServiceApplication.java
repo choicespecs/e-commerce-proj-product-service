9dc5237b6b3fa67dcf848d10e52f3da79a66a89a
Christopher Lee <christophermlee2@gmail.com>
2025-07-23 19:24:49 +0100

    initial commit

---
+++ src/main/java/com/choicespecs/e_commerce_proj_product_service/ECommerceProjProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.choicespecs.e_commerce_proj_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProjProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProjProductServiceApplication.class, args);
+	}
+
+}
