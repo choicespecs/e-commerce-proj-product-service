e77cdc90f2d9b94e4fb72dcea43788b3b44972c8
Christopher Lee <christophermlee2@gmail.com>
2025-07-23 21:51:24 +0100

    entity and repository

--- src/main/java/com/choicespecs/e_commerce_proj_product_service/repository/ProductRepository.java
+++ src/main/java/com/choicespecs/e_commerce_proj_product_service/repository/ProductRepository.java
@@ -5,15 +5,23 @@
 
 package com.choicespecs.e_commerce_proj_product_service.repository;
 
+import java.util.List;
+
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-  
+import org.springframework.stereotype.Repository;
+
 import com.choicespecs.e_commerce_proj_product_service.entity.ProductEntity;  
 
 /**
  *
  * @author metal
  */
+@Repository
 public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
     // Additional query methods can be defined here if needed
+    List<ProductEntity> findAll();
 
+    @Query("SELECT * FROM product WHERE name = :name")
+    ProductEntity findByName(String name);
 }
